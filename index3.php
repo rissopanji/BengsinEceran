beb6c06fc260f076abf7b5597a2b43810a6d136f
rissopanji <rissopanji156@gmail.com>
2022-04-01 15:24:38 +0700

    1

--- index3.php
+++ index3.php
@@ -1,89 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>10120729_Rissopan Panji Prayogi</title>
 
-    <!--CSS -->
-    <link rel="stylesheet" type="text/css" href="./Style.css">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>10120729_Rissopan Panji Prayogi</title>
+
+  <!--CSS -->
+  <link rel="stylesheet" type="text/css" href="./Style.css">
 </head>
+
 <body>
-    <center>
+  <center>
     <h2>Daftar Harga BBM</h2>
     <h4 style="color: red;">10120729_Rissopan Panji Prayogi</h4>
     <br>
     <form action="" method="POST">
-        <table>
+      <table>
 
-            <tr>
-                <th style = "text-align: left">Liter Awal</th>        
-                <th style = "text-align: left">Liter Akhir</th>        
-            </tr>
-            <tr>
-                <td>
-                    <input type="Number" name="Awal">
-                </td>
-                <td>
-                    <input type="Number" name="Akhir">
-                </td>
-                <td>
-                    <button type="submit" name="Submit">View</button>
-                </td>
-            </tr>
-        </table>
-            <?php
-                echo "<br><br>";
-                if (isset($_POST['Submit'])) {
-                    $Awal = $_POST['Awal']; 
-                    $Akhir = $_POST['Akhir'];  
-                    $Premium = 6450;
-                    $Pertalite = 7650;
-                    $Pertamax = 12500;
-                    $Solar = 5150;
+        <tr>
+          <th style="text-align: left">Liter Awal</th>
+          <th style="text-align: left">Liter Akhir</th>
+        </tr>
+        <tr>
+          <td>
+            <input type="Number" name="Awal">
+          </td>
+          <td>
+            <input type="Number" name="Akhir">
+          </td>
+          <td>
+            <button type="submit" name="Submit">View</button>
+          </td>
+        </tr>
+      </table>
+      <?php
+      echo "<br><br>";
+      if (isset($_POST['Submit'])) {
+        $Awal = $_POST['Awal'];
+        $Akhir = $_POST['Akhir'];
+        $Premium = 6450;
+        $Pertalite = 7650;
+        $Pertamax = 12500;
+        $Solar = 5150;
 
-                echo "<table>";
+        echo "<table>";
 
-                echo "<th>Liter</th>";
-                echo "<th>Premium</th>";
-                echo "<th>Pertalite</th>"; 
-                echo "<th>Pertamax</th>";
-                echo "<th>Solar</th>";
+        echo "<th>Liter</th>";
+        echo "<th>Premium</th>";
+        echo "<th>Pertalite</th>";
+        echo "<th>Pertamax</th>";
+        echo "<th>Solar</th>";
 
 
-                    for($i=$Awal; $i <=$Akhir; $i++){
-                      if(($i % 2) == 0){
-                        $bgcolor="white";
-                      } else{
-                        $bgcolor="#e6e6e6";
-                      }
-                        echo " <tr bgcolor=$bgcolor>";
-                        echo "<td>";
-                        echo $i;
-                        echo "</td>";
-                        echo "<td>";
-                        echo "Rp ";
-                        echo $Premium*$i;
-                        echo "</td>";
-                        echo "<td>";
-                        echo "Rp ";
-                        echo $Pertalite*$i;
-                        echo "</td>";
-                        echo "<td>";
-                        echo "Rp ";
-                        echo $Pertamax*$i;
-                        echo "</td>";
-                        echo "<td>";
-                        echo "Rp ";
-                        echo $Solar*$i;
-                        echo "</td>";
-                        echo "</tr>";
-                    }
-
-                }
-                ?>
-                </form>
-    </center>
+        for ($i = $Awal; $i <= $Akhir; $i++) {
+          if (($i % 2) == 0) {
+            $bgcolor = "white";
+          } else {
+            $bgcolor = "#e6e6e6";
+          }
+          echo " <tr bgcolor=$bgcolor>";
+          echo "<td>";
+          echo $i;
+          echo "</td>";
+          echo "<td>";
+          echo "Rp ";
+          echo $Premium * $i;
+          echo "</td>";
+          echo "<td>";
+          echo "Rp ";
+          echo $Pertalite * $i;
+          echo "</td>";
+          echo "<td>";
+          echo "Rp ";
+          echo $Pertamax * $i;
+          echo "</td>";
+          echo "<td>";
+          echo "Rp ";
+          echo $Solar * $i;
+          echo "</td>";
+          echo "</tr>";
+        }
+      }
+      ?>
+    </form>
+  </center>
 </body>
+
 </html>
